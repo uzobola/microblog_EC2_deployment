8950fe7a2aed0bb3b3736b8eb85c78737724a417
Uzo <ubolar@gmail.com>
2024-09-17 05:33:47 +0000

    Trying suggestions for test_App

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import unittest
-from app import create_app, db
+from app import create_app
+from app import db
 from app.models import User, Post
 from config import Config
 
